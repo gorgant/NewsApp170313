4e482a2b757a1479960695e09427ca097ab0a1d2
Greg Ishktar <Greg Ishktar>
2017-03-15 21:45:39 -0400

    Implement the preferences menu in the Main Activity

--- app/src/main/java/com/gorgant/newsapp170313/MainActivity.java
+++ app/src/main/java/com/gorgant/newsapp170313/MainActivity.java
@@ -5,13 +5,17 @@ import android.content.Context;
 import android.content.CursorLoader;
 import android.content.Intent;
 import android.content.Loader;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.CursorAdapter;
@@ -31,8 +35,8 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
      * URL for news article data from the Guardian website
      */
     private static final String GUARDIAN_REQUEST_URL =
-            "http://content.guardianapis.com/search?order-by=newest&use-date=published&api-key=033f7a2f-9a84-40ff-9a25-c02e2104a01f";
-
+            "http://content.guardianapis.com/search?api-key=033f7a2f-9a84-40ff-9a25-c02e2104a01f";
+            //"http://content.guardianapis.com/search?order-by=newest&use-date=published&api-key=033f7a2f-9a84-40ff-9a25-c02e2104a01f"
     /**
      * Constant value for the earthquake loader ID. We can choose any integer.
      * This really only comes into play if you're using multiple loaders.
@@ -122,7 +126,22 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
         Log.i(LOG_TAG,"Loader being created");
 
-        return new NewsArticleLoader(this,GUARDIAN_REQUEST_URL);
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String section = sharedPrefs.getString(
+                getString(R.string.settings_section_key),
+                getString(R.string.settings_section_default));
+
+        String orderBy = sharedPrefs.getString(
+                getString(R.string.settings_order_by_key),
+                getString(R.string.settings_order_by_default));
+
+        Uri baseUri = Uri.parse(GUARDIAN_REQUEST_URL);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        uriBuilder.appendQueryParameter("section", section);
+        uriBuilder.appendQueryParameter("order-by", orderBy);
+
+        return new NewsArticleLoader(this, uriBuilder.toString());
     }
 
     @Override
@@ -150,5 +169,22 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     public void onLoaderReset(Loader<List<NewsArticle>> loader) {
         // Loader reset, so we can clear out our existing data.
         mAdapter.clear();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
